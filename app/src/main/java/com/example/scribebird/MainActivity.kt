d0831f7c40259f821654d16a5a2933346a77973b
DeFauste <39949940+DeFauste@users.noreply.github.com>
2023-05-29 17:04:50 +0300

    develop: bottom menu (b)

--- app/src/main/java/com/example/scribebird/MainActivity.kt
+++ app/src/main/java/com/example/scribebird/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.scribebird
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onStart() {
+        super.onStart()
+        setupActionBarWithNavController(findNavController(R.id.fragment_container))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragment_container)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+
 }
