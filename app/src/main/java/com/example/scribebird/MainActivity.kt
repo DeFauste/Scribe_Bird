e3a0def8f71011a941ae551fdca0c08a67564cb1
Debran <tophadoziratu@gmail.com>
2023-06-20 14:44:13 +0300

    add: BottomNavigation

--- app/src/main/java/com/example/scribebird/MainActivity.kt
+++ app/src/main/java/com/example/scribebird/MainActivity.kt
@@ -4,23 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigateMenuInit()
     }
 
-    override fun onStart() {
-        super.onStart()
-        setupActionBarWithNavController(findNavController(R.id.fragment_container))
+
+    private fun bottomNavigateMenuInit() {
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        val navController = findNavController(R.id.fragmentContainerView)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.fragment_container)
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        val navController = this.findNavController(R.id.bottomNavigationView)
+        return navController.navigateUp()
     }
-
-
 }
